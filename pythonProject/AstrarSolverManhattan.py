23fcd730b71b3aef05526f0b8ab68a5992ca2f47
abdelrahman <abdelrahmanwael50@gmail.com>
2023-10-20 00:48:58 +0300

    Swap characters function loops forever?

--- pythonProject/AstrarSolverManhattan.py
+++ pythonProject/AstrarSolverManhattan.py
@@ -1,3 +1,4 @@
+from pythonProject import Node
 from solverCommand import SolverCommand
 import heapq
 from EightPuzzle import EightPuzzle as ep
@@ -7,50 +8,47 @@ goalState = "012345678"
 
 def mh_heuristic(current_node):
     distance = 0
+    current_node_data = str(current_node.data)
     depth = current_node.depth
     for i in range(9):
-        if current_node[i] != '0':
-            current_row, current_col = divmod(current_node.index(str(i)), 3)
+        if current_node_data[i] != '0':
+            current_row, current_col = divmod(current_node_data.index(str(i)), 3)
             goal_row, goal_col = divmod(goalState.index(str(i)), 3)
             distance += abs(current_row - goal_row) + abs(current_col - goal_col)
             return distance + depth
 
 
 class AStarSolverM(SolverCommand):
-    initial_node = None
-    with_parents = False
-
     def __init__(self, node, with_parents):
-        self.initial_node = node
+        self.initial_node = Node.Node(node, with_parents)
         self.with_parents = with_parents
 
     def execute(self):
-
         frontiers = [(mh_heuristic(self.initial_node), self.initial_node)]
         heapq.heapify(frontiers)
         explored = set()
         parents = {}
 
         while frontiers:
-            state = heapq.heappop(frontiers)
-            explored.add(state[1])
-            if ep.is_goal(state[1]):
-                path_cost = ep.path_cost(state[1])
+            _, state = heapq.heappop(frontiers)
+            explored.add(state)
+            if ep.is_goal(state):
+                path_cost = ep.path_cost(state)
                 nodes_expanded = ep.nodes_expanded(explored)
                 if self.with_parents:
-                    path = ep.path_to_goal(state[1], parents)
+                    path = ep.path_to_goal(state, parents)
                     return "Success", (path_cost, nodes_expanded, path)
                 else:
                     return "Success", (path_cost, nodes_expanded)
-            neighbours = ep.actions(state[1])
-            for neighbor in neighbours:
+            neighbors = ep.actions(state)
+            new_frontiers = []
+            for neighbor in neighbors:
                 if neighbor not in explored:
                     if neighbor not in [s[1] for s in frontiers]:
-                        heapq.heappush(frontiers, (mh_heuristic(neighbor), neighbor))
+                        new_frontiers.append((mh_heuristic(neighbor), neighbor))
                         if self.with_parents:  # Check if path required or not
-                            parents[neighbor] = state[1].data
-                    else:
-                        frontiers = [(h, s) if s != neighbor else (min(h, mh_heuristic(neighbor)), neighbor) for h, s in
-                                     frontiers]
-                        heapq.heapify(frontiers)
+                            parents[neighbor] = state.data
+            frontiers.extend(new_frontiers)
+            heapq.heapify(frontiers)
         return "Fail"
+
--- pythonProject/Node.py
+++ pythonProject/Node.py
@@ -1,5 +1,12 @@
-
 class Node:
     def __init__(self, data, depth):
-        self.data = data
+        self.data = str(data)
         self.depth = depth
+
+    def __lt__(self, other):
+        # Define how to compare two Node instances
+        return self.depth < other.depth  # Compare based on depth for A* search
+
+    def __hash__(self):
+        # Define a hash function for the Node class
+        return hash((self.data, self.depth))
