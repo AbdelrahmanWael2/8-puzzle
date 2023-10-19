69518562141c5cf4d45085f9083d1ed5a86ba854
Mohamed <mohamedraffeek2001@gmail.com>
2023-10-19 20:47:39 +0300

    Initial commit, implemented the possible actions function

--- pythonProject/EightPuzzle.py
+++ pythonProject/EightPuzzle.py
@@ -2,16 +2,56 @@ from abc import ABC
 
 
 # the implementation of the interface
+
+def swap_characters(s, index1, index2):
+    # Convert the string to a list to make swapping easier
+    string_list = list(s)
+
+    # Swap the characters at the given indices
+    string_list[index1], string_list[index2] = string_list[index2], string_list[index1]
+
+    # Convert the list back to a string and return it
+    return ''.join(string_list)
+
+
 class EightPuzzle:
 
     @staticmethod
     def is_goal(self, state):
         # Define the goal state checking logic
+        if state == "012345678":
+            return True
+        return False
         pass
 
     @staticmethod
     def actions(self, state):
         # Define the actions available for a given state
+        pos = state.find('0')
+        down = pos + 3
+        up = pos - 3
+        right = pos + 1
+        left = pos - 1
+        row = pos // 3
+        col = pos % 3
+        possible_actions = []
+        if col < 2:
+            possible = state
+            swap_characters(possible, pos, right)
+            possible_actions.append(possible)
+        if row < 2:
+            possible = state
+            swap_characters(possible, pos, down)
+            possible_actions.append(possible)
+        if col > 0:
+            possible = state
+            swap_characters(possible, pos, left)
+            possible_actions.append(possible)
+        if row > 0:
+            possible = state
+            swap_characters(possible, pos, up)
+            possible_actions.append(possible)
+        return possible_actions
         pass
 
     @staticmethod
