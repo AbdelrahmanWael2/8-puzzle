abc2e5b3a1e386156841a7f547ff0c3412ca5ed5
Hussein Mohamed <96450583+hussainmansour@users.noreply.github.com>
2023-10-23 23:46:11 +0300

    resolve conversions

--- pythonProject/BFSSolver.py
+++ pythonProject/BFSSolver.py
@@ -1,5 +1,6 @@
 from solverCommand import SolverCommand
 import EightPuzzle as ep
+import Node
 
 
 class BFSSolver(SolverCommand):
@@ -8,10 +9,9 @@ class BFSSolver(SolverCommand):
     depth = 0
 
     def __init__(self, node, with_parents):
-        self.initial_node = node
+        self.initial_node = Node.Node(node.data, 0)
         self.with_parents = with_parents
         self.depth = 0
-
 
     def execute(self):
         # Implement BFS algorithm for solving the puzzle
@@ -24,16 +24,14 @@ class BFSSolver(SolverCommand):
         frontier_hash[self.initial_node.data] = True
 
         # Initialize the parents dictionary for printing the path to the goal
-        parents = {self.initial_node.data: None}
+        parents = {self.initial_node.data: self.initial_node.data}
         while frontier:
             state = frontier.pop(0)
             explored.add(state.data)
             if ep.is_goal(state.data):  # Reached 012345678
-                if self.with_parents:
-                    return ("success", ep.path_cost(state), ep.nodes_expanded(explored), self.depth,
-                            ep.path_to_goal(state, parents))
-                else:
-                    return "success", ep.path_cost(state), ep.nodes_expanded(explored), self.depth
+
+                return ("success", ep.path_cost(state), ep.nodes_expanded(explored), self.depth,
+                        ep.path_to_goal(state, parents))
 
             # Get the possible actions for the current state
             possible_actions = ep.actions(state)
@@ -44,6 +42,3 @@ class BFSSolver(SolverCommand):
                     parents[action.data] = state.data
                     self.depth = max(self.depth, action.depth)
         return "fail", 0, 0, 0, []
-
-
-
